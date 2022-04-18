c8d1499dffbf7590bc9fecc71f54745ed0f3c8ae
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-04-17 23:46:36 -0300

    Change attrs by raft paper

--- pkg/node/node.go
+++ pkg/node/node.go
@@ -2,6 +2,14 @@ package node
 
 import "github.com/octopipe/raft/pkg/server"
 
+type NodeState uint32
+
+const (
+  Follower NodeState = iota
+  Candidate
+  Leader
+)
+
 type Node struct {
   server.Server
   currentTerm int
