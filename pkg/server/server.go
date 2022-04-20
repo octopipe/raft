d6ac4f854394d89d87fad358db5c5afdcc4d0087
Maycon Pacheco <mayconjrpacheco@gmail.com>
2022-04-20 00:02:40 -0300

    Create grpc server start

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,19 +1,37 @@
 package server
 
+import (
+	"net"
+
+	"google.golang.org/grpc"
+  raftPb "github.com/octopipe/raft/proto/raft/v1"
+)
+
 type Server struct {
+  raftPb.UnimplementedRaftServer
   ID string
-  host string
-  port int
+  address string
   timer int
 }
 
 func NewServer(host string, port int) Server {
   newServer := Server{
     ID: "",
-    host: host,
-    port: port,
+    address: "",
     timer: 0,
   }
 
   return newServer
 }
+
+func (s Server) Start() error {
+  lis, err := net.Listen("tcp", s.address)
+  if err != nil {
+    return err
+  }
+  grpcServer := grpc.NewServer()
+  raftPb.RegisterRaftServer(grpcServer, s)
+  return grpcServer.Serve(lis)
+
+}
+
